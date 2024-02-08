57b41eebe411afd347db438067fbdc7d1eb2511a
celina.balz <celina.balz@ufj.edu.br>
2024-02-08 16:00:39 -0300

    feat: estilização e componentização new note card

--- src/components/note-card.tsx
+++ src/components/note-card.tsx
@@ -1,4 +1,7 @@
 import * as Dialog from '@radix-ui/react-dialog'
+import {formatDistanceToNow} from 'date-fns'
+import { ptBR } from 'date-fns/locale'
+import { X } from 'lucide-react'
 
 interface NoteCardProps{
     note: {
@@ -22,8 +25,24 @@ export function NoteCard({note}: NoteCardProps) {
             
             <Dialog.Portal>
                 <Dialog.Overlay className='inset-0 fixed bg-black/50'></Dialog.Overlay>
-                <Dialog.Content className='fixed left-1/2 top-1/2  -translate-x-1/2 -translate-y-1/2 max-w-[640px] w-full bg-slate-700 rounded-md flex flex-col outline-none'>
-                    <div className='flex flex-1'></div>
+                <Dialog.Content className='fixed left-1/2 top-1/2 overflow-hidden -translate-x-1/2 -translate-y-1/2 max-w-[640px] h-[600px] w-full bg-slate-700 rounded-md flex flex-col outline-none'>
+                  <Dialog.DialogClose className='absolute top-0 right-0 bg-slate-800 p-1.5 text-slate-400 hover:text-slate-100'>
+                    <X className='size-5'/>
+                  </Dialog.DialogClose>
+                    <div className='flex flex-1 flex-col gap-3 p-5'>
+                        <span className='text-sm font-medium text-slate-300'>
+                            {formatDistanceToNow(note.date, {locale: ptBR, addSuffix: true })}
+                        </span> 
+
+                        <p className='text-sm leading-6 text-slate-400'>
+                            {note.content}
+                        </p>                    
+                    </div>
+                    <button 
+                    type="button"
+                     className='group w-full font-medium bg-slate-800 py-4 text-center text-sm text-slate-300 outline-none'>
+                        Deseja <span className='group-hover:underline text-red-400 hover:underline'> apagar essa nota?</span>
+                    </button>
                 </Dialog.Content>
             </Dialog.Portal>
             
